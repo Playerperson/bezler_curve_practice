bf995db87d7eaf65f8b74e041e4a4a08246a299c
Nevermore <147987693+Playerperson@users.noreply.github.com>
2026-06-20 05:15:21 +0400

    Create heavy armor item with attributes
    Added a heavy armor item with detailed properties and resistances.

---
+++ main.c
@@ -0,0 +1,24 @@
+ITEM.name = "Комплект тяжелой брони"
+ITEM.description = "Самый крепкий и тяжелый комплект, который используется для снаряжения пулеметчиков. Имеет хорошую защиту от осколков, большинства калибров, но тем не менее - из-за плотности вес выше, что затрудняет передвижение."
+ITEM.model = "models/ney/tarkov/barter/item_barter_container_parls_lod0_0.mdl"
+ITEM.width = 3
+ITEM.height = 3
+ITEM.armorAmount = 0
+ITEM.gasmask = false -- It will protect you from bad air
+ITEM.category = "Экипировка"
+ITEM.resistance = true -- This will activate the protection bellow
+
+ITEM.moveSpeedMult = 0.25
+ITEM.runSpeedMult = 0.25
+ITEM.jumpPowerMult = 0.25
+ITEM.coldResistance = 100
+
+ITEM.damage = { -- It is scaled; so 100 damage * 0.8 will makes the damage be 80.
+			0.6, -- Bullets
+			0.65, -- Slash
+			0.65, -- Shock
+			1, -- Burn
+			0, -- Radiation
+			0.65, -- Acid
+			1, -- Explosion
+}
